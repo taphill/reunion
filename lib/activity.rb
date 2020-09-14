5ad5377cfd4067a868bbf43f3f44abd35d38e712
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-14 09:53:01 -0500

    Update Activity class

---
+++ lib/activity.rb
@@ -0,0 +1,17 @@
+class Activity
+  attr_reader :name, :participants 
+
+  def initialize(name)
+    @name = name
+    @participants = {} 
+  end
+
+  def add_participant(name, amount_paid)
+    participants[name] = amount_paid
+  end
+
+  def total_cost
+    participants.values.sum 
+  end
+end
+
---
+++ test/activity_test.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/activity.rb'
+
+class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new("Brunch")
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_attributes
+    activity = Activity.new("Brunch")
+
+    assert_equal 'Brunch', activity.name
+    assert_equal({}, activity.participants)
+  end
+
+  def test_it_can_add_participant
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+
+    assert_equal({"Maria" => 20}, activity.participants)
+  end
+
+  def test_it_can_calculate_total_cost
+    activity = Activity.new("Brunch")
+    activity.add_participant("Maria", 20)
+
+    assert_equal 20, activity.total_cost
+
+    activity.add_participant("Luther", 40)
+
+    assert_equal 60, activity.total_cost
+  end
+end
