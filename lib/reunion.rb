e4e4b30b8146d7c734af5eb53eb9e6c2403d703b
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-15 09:43:16 -0500

    Create breakout method

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,7 +1,29 @@
 class Reunion
-  attr_reader :name 
+  attr_reader :name, :activities
 
   def initialize(name)
     @name = name
+    @activities = []
+  end
+
+  def add_activity(activity)
+    activities << activity
+  end
+
+  def total_cost
+    activities.sum do |activity|
+      activity.total_cost
+    end
+  end
+
+  def breakout
+    breakout_costs = Hash.new(0)
+    activities.each do |activity|
+      activity.owed.each do |participant, amount_owed|
+        breakout_costs[participant] += amount_owed
+      end
+    end
+
+    breakout_costs
   end
 end
