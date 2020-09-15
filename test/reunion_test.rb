bce6eb2e0a89b8239b9ada5b40527003e3d18342
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-15 09:56:29 -0500

    Add test for summary method

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -49,4 +49,132 @@ class ReunionTest < Minitest::Test
 
     assert_equal expected, reunion.breakout
   end
+
+  def test_it_can_print_summary
+    reunion = Reunion.new("1406 BE")
+    activity_1 = Activity.new("Brunch")
+
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+    reunion.add_activity(activity_1)
+
+    assert_equal 60, reunion.total_cost
+
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+    reunion.add_activity(activity_2)
+
+    assert_equal 180, reunion.total_cost
+
+    assert_equal "Maria: -10\nLuther: -30\nLouis: 40", reunion.summary
+
+    puts reunion.summary
+  end
+
+  def test_it_can_prove_detailed_breakout
+    reunion = Reunion.new("1406 BE")
+
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Maria", 20)
+    activity_1.add_participant("Luther", 40)
+
+    # One person owes two people
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+
+    # Two people owe one person
+    activity_3 = Activity.new("Bowling")
+    activity_3.add_participant("Maria", 0)
+    activity_3.add_participant("Luther", 0)
+    activity_3.add_participant("Louis", 30)
+
+    # Two people owe two people
+    activity_4 = Activity.new("Jet Skiing")
+    activity_4.add_participant("Maria", 0)
+    activity_4.add_participant("Luther", 0)
+    activity_4.add_participant("Louis", 40)
+    activity_4.add_participant("Nemo", 40)
+
+    reunion.add_activity(activity_1)
+    reunion.add_activity(activity_2)
+    reunion.add_activity(activity_3)
+    reunion.add_activity(activity_4)
+
+    expected = {
+      "Maria" => [
+        {
+          activity: "Brunch",
+          payees: ["Luther"],
+          amount: 10
+        },
+        {
+          activity: "Drinks",
+          payees: ["Louis"],
+          amount: -20
+        },
+        {
+          activity: "Bowling",
+          payees: ["Louis"],
+          amount: 10
+        },
+        {
+          activity: "Jet Skiing",
+          payees: ["Louis", "Nemo"],
+          amount: 10
+        }
+      ],
+      "Luther" => [
+        {
+          activity: "Brunch",
+          payees: ["Maria"],
+          amount: -10
+        },
+        {
+          activity: "Drinks",
+          payees: ["Louis"],
+          amount: -20
+        },
+        {
+          activity: "Bowling",
+          payees: ["Louis"],
+          amount: 10
+        },
+        {
+          activity: "Jet Skiing",
+          payees: ["Louis", "Nemo"],
+          amount: 10
+        }
+      ],
+      "Louis" => [
+        {
+          activity: "Drinks",
+          payees: ["Maria", "Luther"],
+          amount: 20
+        },
+        {
+          activity: "Bowling",
+          payees: ["Maria", "Luther"],
+          amount: -10
+        },
+        {
+          activity: "Jet Skiing",
+          payees: ["Maria", "Luther"],
+          amount: -10
+        }
+      ],
+      "Nemo" => [
+        {
+          activity: "Jet Skiing",
+          payees: ["Maria", "Luther"],
+          amount: -10
+        }
+      ]
+    }
+
+    assert_equal expected, reunion.detailed_breakout
+  end
 end
